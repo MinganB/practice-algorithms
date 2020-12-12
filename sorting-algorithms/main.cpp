5aeb9a8bdbd150c396e0a61b781bd6a68003c3bf
MinganB <contact@mingan.co.za>
2020-12-12 17:17:15 +0200

    Finsihed insertion sorting and fixed selection sorting

--- sorting-algorithms/main.cpp
+++ sorting-algorithms/main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 void bubbleSort() {
     int n;
-    int totalInnerLoops = 0, totalOuterLoops = 0;
     
     cout << "Enter size of list to sort: ";
     cin >> n;
@@ -27,7 +26,6 @@ void bubbleSort() {
                 sortList[i - 1] = smallest;
                 exchangeMade = true;
             }
-            totalInnerLoops++;
         }
         
         sortPasses++;
@@ -43,7 +41,6 @@ void bubbleSort() {
         cout << " " << sortList[i];
     
     cout << endl << "Sort Passes: " << sortPasses;
-    cout << endl << "Total Inner Loops: " << totalInnerLoops;
 }
 
 void selectionSort() {
@@ -58,25 +55,23 @@ void selectionSort() {
         cin >> sortList[i];
     
     //Selection sort algorithm
-    int unsortedListStartLoc = 0; //wall
-    int sortPasses = 0, totalInnerLoops = 0;
+    int unsortedStart = 0; //wall
+    int sortPasses = 0;
     
-    while(unsortedListStartLoc < n - 1) {
-        int smallest = INT_MAX;
-        int smallestLoc = 0;
-        
-        for(int i = unsortedListStartLoc; i < n - 1; i++) {
-            if(sortList[i] < smallest) {
-                smallest = sortList[i];
-                smallestLoc = i;
+    while(unsortedStart < n - 1) {
+        int currentMin = sortList[unsortedStart], currentMinLoc = unsortedStart;
+        for(int i = unsortedStart; i < n; i++) {
+            if(sortList[i] < currentMin) {
+                currentMin = sortList[i];
+                currentMinLoc = i;
             }
-            totalInnerLoops++;
         }
         
-        int k = sortList[unsortedListStartLoc];
-        sortList[unsortedListStartLoc] = smallest;
-        sortList[smallestLoc] = k;
-        unsortedListStartLoc++;
+        int t = sortList[unsortedStart];
+        sortList[unsortedStart] = currentMin;
+        sortList[currentMinLoc] = t;
+        
+        unsortedStart++;
         sortPasses++;
     }
     
@@ -86,17 +81,49 @@ void selectionSort() {
         cout << " " << sortList[i];
     
     cout << endl << "Sort Passes: " << sortPasses;
-    cout << endl << "Total Inner Loops: " << totalInnerLoops;
 }
 
 void insertionSort() {
+    int n;
     
+    cout << "Enter size of list to sort: ";
+    cin >> n;
+    int sortList[n];
+    
+    cout << "Enter list of elements to sort seperated by spaces: ";
+    for(int i = 0; i < n; i++)
+        cin >> sortList[i];
+    
+    int sortPasses = 0;
+    
+    //Insertion sort algorithm
+    int unsortedListStartLoc = 1;
+    while(unsortedListStartLoc < n) {
+        for(int i = unsortedListStartLoc; i > 0; i--) {
+            if(sortList[i] < sortList[i - 1]) {
+                int t = sortList[i - 1];
+                sortList[i - 1] = sortList[i];
+                sortList[i] = t;
+            }
+        }
+        
+        sortPasses++;
+        unsortedListStartLoc++;
+    }
+    
+    //display the sorted list
+    cout << endl << "Sorted list:";
+    for(int i = 0; i < n; i++)
+        cout << " " << sortList[i];
+    
+    cout << endl << "Sort Passes: " << sortPasses;
 }
 
 int main() {
     cout << "Choose a sort type to run:" << endl;
     cout << "1) Selection Sort" << endl;
     cout << "2) Bubble Sort" << endl;
+    cout << "3) Insertion Sort" << endl;
     cout << "Sort type: ";
     
     int sortType;
